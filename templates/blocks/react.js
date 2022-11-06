4976473799454573bf22259cc887f5a7ce4f774b
Gabriel Lantz <gabriel.lantz@svt.se>
2022-11-06 22:47:54 +0100

    wip

--- templates/blocks/react.js
+++ templates/blocks/react.js
@@ -2,7 +2,7 @@ const fooaSectionTest = document.createElement('section');
 fooaSectionTest.id = '{{block_id}}';
 fooaSectionTest.classList.add('{{template_id}}');
 fooaSectionTest.classList.add('block');
-document.querySelector('.full').appendChild(fooaSectionTest);
+parent.frames['clientframe'].contentWindow.querySelector('.full').appendChild(fooaSectionTest);
 // document.querySelector('.full').appendChild('<section id="{{block_id}}" class="{{template_id}} block"></section>');
 function KlarBlock({heading, subHeading, image, team, text}) {
   return (
